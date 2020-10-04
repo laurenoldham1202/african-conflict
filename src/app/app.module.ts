3ab18b46764ce92bf30132f4ba7a395a32e96a7c
lauren.oldham@ursaspace.com <laurenoldham1202@gmail.com>
2020-10-04 19:41:56 -0400

    Initialize project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+    SidePanelComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
